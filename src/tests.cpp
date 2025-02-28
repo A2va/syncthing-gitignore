42f4b9261f872d942759dde46b8cdf390847e6e3
A2va <49582555+A2va@users.noreply.github.com>
2025-02-28 11:12:00 +0100

    Update tests.cpp

--- src/tests.cpp
+++ src/tests.cpp
@@ -40,6 +40,22 @@ class TemporaryDirectory {
         fs::path path;
 };
 
+TEST_CASE("simple win") {
+    TemporaryDirectory temp_dir;
+    fs::path gitignore_path = temp_dir.get_path() / ".gitignore";
+    {
+        std::ofstream file(gitignore_path);
+        file << "__pycache__/\n";
+        file << "*.py[cod]";
+    }
+    GitIgnoreMatcher matcher(gitignore_path, "D:\\home\\a2va");
+ 
+    CHECK_FALSE(matcher.is_ignored("D:\\home\\a2va\\main.py"));
+    CHECK(matcher.is_ignored("D:\\home\\a2va\\main.pyc"));
+    CHECK(matcher.is_ignored("D:\\home\\a2va\\dir\\main.pyc"));
+    CHECK(matcher.is_ignored("D:\\home\\a2va\\__pycache__"));
+ }
+
 TEST_CASE("simple") {
    TemporaryDirectory temp_dir;
    fs::path gitignore_path = temp_dir.get_path() / ".gitignore";
