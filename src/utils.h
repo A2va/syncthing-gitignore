b13f027823f3bbe57f9a94437855b287ba03c085
A2va <49582555+A2va@users.noreply.github.com>
2025-03-05 16:08:56 +0100

    Update utils.h

--- src/utils.h
+++ src/utils.h
@@ -2,6 +2,15 @@
 #define UTILS_H
 #include "tbox/tbox.h"
 
+#ifdef __cplusplus
+    extern "C" {
+#endif
+
 tb_char_t const* get_program_file();
 tb_char_t const* get_sys_name();
+
+#ifdef __cplusplus
+    }
+#endif
+
 #endif
