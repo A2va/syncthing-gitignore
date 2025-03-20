846eca6134cd5b441f60243b123ca312f5710200
A2va <49582555+A2va@users.noreply.github.com>
2025-03-20 12:42:58 +0100

    Check file write time before computing

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
@@ -14,11 +15,35 @@
 using json = nlohmann::json;
 namespace fs = std::filesystem;
 
+namespace nlohmann
+{
+	template<> struct adl_serializer<std::filesystem::file_time_type>
+	{
+		static void to_json(json& j, const std::filesystem::file_time_type& time)
+		{
+			// Store the time as duration since epoch
+			auto duration = time.time_since_epoch();
+			auto count = duration.count();
+			j = count;
+		}
+
+		static void from_json(const json& j, std::filesystem::file_time_type& time)
+		{
+			// Read the duration count from JSON
+			auto count = j.get<typename std::filesystem::file_time_type::duration::rep>();
+
+			// Create a duration and then a time_point from it
+			typename std::filesystem::file_time_type::duration duration(count);
+			time = std::filesystem::file_time_type(duration);
+		}
+	};
+} // namespace nlohmann
+
 struct Config
 {
 	std::set<std::string> synctignore_rules;
 	std::set<std::string> user_rules;
-	std::set<fs::path> gitignore_files;
+	std::map<fs::path, fs::file_time_type> gitignore_files;
 	NLOHMANN_DEFINE_TYPE_INTRUSIVE(Config, synctignore_rules, user_rules, gitignore_files);
 
 	static Config load()
@@ -73,23 +98,25 @@ void strip(std::string& str)
 	str = start_pos <= end_pos ? std::string(start_it, end_it.base()) : "";
 }
 
-std::set<fs::path> collect_gitignore_files(const fs::path& path)
+std::map<fs::path, fs::file_time_type> collect_gitignore_files(const fs::path& path)
 {
-	std::set<fs::path> gitignore_files;
+	std::map<fs::path, fs::file_time_type> gitignore_files;
 	fs::recursive_directory_iterator dir_iter(normalize_path(path));
 	for (const auto& entry : dir_iter)
 	{
 		std::cout << entry.path() << std::endl;
 		if (entry.path().filename() == ".gitignore")
 		{
-			gitignore_files.insert(entry.path());
+			const auto mtime = entry.last_write_time();
+			gitignore_files.emplace(entry.path(), mtime);
 		}
 	}
 	return gitignore_files;
 }
 
 // Convert ignore rules from git to syncthing
-std::set<std::string> convert_ignore_rules(const std::set<fs::path>& gitignore_files)
+std::set<std::string> convert_ignore_rules(
+	const std::map<fs::path, fs::file_time_type>& gitignore_files)
 {
 	std::vector<GitIgnoreMatcher> matchers;
 	const auto matches = [&matchers](const fs::path& path) {
@@ -104,19 +131,20 @@ std::set<std::string> convert_ignore_rules(const std::set<fs::path>& gitignore_f
 	};
 
 	std::set<std::string> ignore_rules;
-	for (const auto& file_path : gitignore_files)
+	for (const auto& file : gitignore_files)
 	{
+		const auto file_path = file.first;
 		if (matches(file_path))
 			continue;
 
 		matchers.emplace_back(file_path, normalize_path(fs::current_path()));
 
 		fs::path gitignore_parent_path = fs::relative(file_path.parent_path()).lexically_normal();
-		std::ifstream file(file_path);
+		std::ifstream ifs(file_path);
 		int line_num = 0;
 		std::string line;
 
-		while (std::getline(file, line))
+		while (std::getline(ifs, line))
 		{
 			line_num++;
 			strip(line);
@@ -152,13 +180,14 @@ std::set<std::string> convert_ignore_rules(const std::set<fs::path>& gitignore_f
 
 void save_stignore(const Config& config)
 {
-	std::ofstream ofs("stignore", std::ios::out);
+	std::ofstream ofs(".stignore", std::ios::out);
 	for (const auto& rule : config.synctignore_rules)
 	{
 		ofs << rule << "\n";
 	}
 
-	ofs << "# USER RULES" << "\n";
+	ofs << "# USER RULES"
+		<< "\n";
 
 	for (const auto& rule : config.user_rules)
 	{
@@ -169,27 +198,26 @@ void save_stignore(const Config& config)
 tb_int_t main(tb_int_t argc, tb_char_t** argv)
 {
 	const auto executable_directory = normalize_path(fs::path(get_program_file()).parent_path());
-	std::set<std::string> ignore_rules;
 
 	// Load config
 	Config config = Config::load();
 
 	// First stignore creation if it doesn't exist
-	if (!fs::exists(executable_directory / "stignore"))
+	if (!fs::exists(".stignore"))
 	{
 		const auto gitignore_files = collect_gitignore_files(executable_directory);
 		config.synctignore_rules = convert_ignore_rules(gitignore_files);
+		config.gitignore_files = gitignore_files;
 		save_stignore(config);
 		config.save();
 	}
 	else
 	{
-		std::ifstream ifs("stignore");
+		std::ifstream ifs(".stignore");
 		int line_num = 0;
 		std::string line;
 
 		std::set<std::string> rules;
-
 		while (std::getline(ifs, line))
 		{
 			line_num++;
@@ -205,11 +233,34 @@ tb_int_t main(tb_int_t argc, tb_char_t** argv)
 				config.user_rules.insert(rule);
 			}
 		}
-		save_stignore(config);
-	}
 
-	for (const auto& rule : ignore_rules)
-	{
-		std::cout << rule << std::endl;
+		// Check if some gitignore files were modified, update stignore rules accordingly
+		const auto gitignore_files = collect_gitignore_files(executable_directory);
+		std::map<fs::path, fs::file_time_type> updated_gitignore;
+		for (const auto& file : gitignore_files)
+		{
+
+			const auto existing_file_entry = config.gitignore_files.find(file.first);
+			// New discovered file
+			if (existing_file_entry == config.gitignore_files.end())
+			{
+				updated_gitignore.insert(file);
+				continue;
+			}
+
+			// File was modified
+			if (file.second > existing_file_entry->second)
+			{
+				updated_gitignore.insert(file);
+				config.gitignore_files.erase(existing_file_entry);
+			}
+		}
+
+		// Update the config with the updated files/ rules
+		config.synctignore_rules.merge(convert_ignore_rules(updated_gitignore));
+		config.gitignore_files.merge(updated_gitignore);
+
+		config.save();
+		save_stignore(config);
 	}
 }
