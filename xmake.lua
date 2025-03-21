c53961da2df33e3272931dd802a17c3fac051227
A2va <49582555+A2va@users.noreply.github.com>
2025-03-21 14:29:22 +0100

    Patch tbox to support pipe with cosmocc

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,19 @@
 add_rules("mode.debug", "mode.release")
 set_languages("cxx20")
 
-add_requires("tbox", {configs = {charset = true}})
+package("tbox-patched")
+    set_base("tbox")
+    -- add_patches("v1.7.6", "https://github.com/tboox/tbox/commit/bf717ac71bc6b7c95695d6a2c9b0c8dc11331cb9.patch", "530e78af853b5b8d80a71dbe9463a604bee55e6107415d74b53ed7a7bfd59121")
+    on_load(function (package)
+        if package:has_tool("cc", "cosmocc") then
+            package:add("patches", "v1.7.6", "https://github.com/tboox/tbox/commit/bf717ac71bc6b7c95695d6a2c9b0c8dc11331cb9.patch", "530e78af853b5b8d80a71dbe9463a604bee55e6107415d74b53ed7a7bfd59121")
+        end
+    end)
+
+package_end()
+
+-- enable micro tbox because cosmocc patch will become bigger without it
+add_requires("tbox", {configs = {charset = true, micro = true}})
 add_requires("doctest", "nlohmann_json")
 
 target("gitignore_parser")
